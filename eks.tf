0341407cf3ec5b483230a63c666ace547ee6b039
vishwanath2390 <113889125+vishwanathkit7639@users.noreply.github.com>
2026-06-11 16:47:21 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -35,7 +35,7 @@ module "eks" {
       max_size     = 2
       desired_size = 1
 
-      instance_types = ["t2.medium"]
+      instance_types = ["t3.small"]
       capacity_type  = "SPOT"
 
       tags = {
