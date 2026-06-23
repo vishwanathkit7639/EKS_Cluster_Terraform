629e71aa7cbe2bf68529d1bd183c7dcfb1674f1e
vishwanath2390 <113889125+vishwanathkit7639@users.noreply.github.com>
2026-06-23 12:31:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,19 +9,19 @@ terraform {
   backend "s3" {
     bucket = "vishwa7639"
     key    = "vishwanath/terraform.tfstate"
-    region = "us-east-1"   
+    region = "ap-south-1"   
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 locals {
-  region = "us-east-1"
+  region = "ap-south-1"
   name = "vgs_cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["ap-south-1a", "south-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
